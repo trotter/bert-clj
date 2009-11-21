f1c76ae3a6c83c76844687dabb48aad3fe05e8cd
Trotter Cashion <cashion@gmail.com>
2009-11-21 01:28:26 -0800

    So much better when you only say it once

--- src/main/clojure/com/trottercashion/bert_clj/encoder.clj
+++ src/main/clojure/com/trottercashion/bert_clj/encoder.clj
@@ -3,21 +3,21 @@
 (def *version* (byte 131))
 
 (def *types* 
-  { java.lang.String (byte 107)
-    java.lang.Double (byte 99)
-    :small-int       (byte 97)})
+  { java.lang.String 107
+    java.lang.Double 99
+    :small-int       97})
 
 (defn bytes [& args]
   (map #(.toByte %) args))
 
 (defn twoByteLength [bytes]
   (let [size (count bytes)
-        second-byte (byte (bit-and 255 size))
-        first-byte (byte (bit-shift-right size 8))]
+        second-byte (bit-and 255 size)
+        first-byte (bit-shift-right size 8)]
     [first-byte second-byte]))
 
 (defn coerce [kind & args]
-  (concat [*version* (*types* kind)] (apply concat args)))
+  (map byte (concat [*version* (*types* kind)] (apply concat args))))
 
 (defmulti encode #(type %))
 
@@ -31,6 +31,6 @@
 
 (defmethod encode java.lang.Integer [i]
   (if (< i 256)
-    (coerce :small-int [(byte i)])))
+    (coerce :small-int [i])))
 
 
