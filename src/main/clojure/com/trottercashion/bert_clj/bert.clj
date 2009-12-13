4a454dad6426dca16dbad2d997c1fb34361c1214
Trotter Cashion <cashion@gmail.com>
2009-12-13 01:40:32 -0500

    Full on encoding/decoding

--- src/main/clojure/com/trottercashion/bert_clj/bert.clj
+++ src/main/clojure/com/trottercashion/bert_clj/bert.clj
@@ -1,6 +1,14 @@
 (ns com.trottercashion.bert-clj.bert
   (:require [com.trottercashion.bert-clj.etf-encoder  :as etf-encoder]
-            [com.trottercashion.bert-clj.bert-encoder :as bert-encoder]))
+            [com.trottercashion.bert-clj.bert-encoder :as bert-encoder]
+            [com.trottercashion.bert-clj.etf-decoder  :as etf-decoder]
+            [com.trottercashion.bert-clj.bert-decoder :as bert-decoder]))
 
 (defn encode [obj]
   (etf-encoder/encode (bert-encoder/encode obj)))
+
+(defn decode [coll]
+  (let [etf-decoded (etf-decoder/decode coll)]
+    (try
+     (bert-decoder/decode etf-decoded)
+     (catch Exception _ etf-decoded))))
--- src/test/clojure/com/trottercashion/bert_clj/bert_test.clj
+++ src/test/clojure/com/trottercashion/bert_clj/bert_test.clj
@@ -4,9 +4,18 @@
             [com.trottercashion.bert-clj.etf-encoder :as etf-encoder]
             [com.trottercashion.bert-clj.bert-encoder :as bert-encoder]))
 
+(defn test-round-trip [val]
+  (is (= (bert/decode (bert/encode val)) val)))
+
 (deftest should-encode-etf-type
   (is (= (bert/encode "hello") (etf-encoder/encode "hello"))))
 
 (deftest should-encode-complex-types
   (is (= (bert/encode {"one key" "two key" "red key" "blue key"})
          (etf-encoder/encode (bert-encoder/encode {"one key" "two key" "red key" "blue key"})))))
+
+(deftest should-decode-etf-type
+  (test-round-trip "hello"))
+
+(deftest should-decode-complex-types
+  (test-round-trip {"one key" "two key" "red key" "blue key"}))
